04e2d7bb5cfcc1435b0b8e7f2c08310c05427366
Ivan Davidov <davidov.i@gmil.com>
2018-06-30 00:33:54 +0300

    Added sample 'exec' function.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# nodemcu
-NodeMCU - ESP8266
+# Experiments with ESP8266 and NodeMCU
+
+* General purpose 'telnet' server.
+* Control the built-in LED via simple web page.
+* Simple ``exec`` function to compile and run LUA scripts on demand.
--- init.lua
+++ init.lua
@@ -1,2 +1,26 @@
-dofile('telnet.lua')
-dofile('program.lua')
+function exec(filename)
+  if (filename == nil) then
+    filename = ''
+  end
+
+  filename = filename:gsub('%s+', '')
+  
+  if (filename == '') then
+    error('Error - no file has been provided.')    
+  end
+
+  local filelc = filename .. '.lc'
+  local filelua = filename .. '.lua'
+  
+  if (file.exists(filelc)) then
+    dofile(filelc)
+  elseif (file.exists(filelua)) then
+    node.compile(filelua)
+    dofile(filelc)
+  else
+    error('Provided file does not exist.')
+  end
+end
+
+exec('telnet')
+exec('program')
